6bf8db78c0f533015b625d8eb7cfd79496c1a12e
Firmansyah Yanuar <fyfirman@gmail.com>
2020-10-27 00:39:47 +0700

    feat: handling error

--- src/helpers/error.ts
+++ src/helpers/error.ts
@@ -12,16 +12,22 @@ export class RestError extends Error {
     this.message = message;
   }
 
-  public getStatusCode = () : number => this.statusCode
+  public getStatusCode = (): number => this.statusCode;
 }
 
-export const errorHandler = (error, req: Request, res: Response, next: NextFunction) : void => {
+export const errorHandler = (error, req: Request, res: Response, next: NextFunction): void => {
   const logger: Logger = Container.get('logger');
 
   if (!isCelebrate(error)) {
-    const statusCode = error.getStatusCode() ? error.getStatusCode() : 500;
-    res.status(statusCode).json({ success: false, message: error.message });
-    logger.error(`${req.method} ${req.originalUrl} | ${statusCode} ${error.message}`);
+    if (error.getStatusCode !== undefined) {
+      const statusCode = error.getStatusCode() ? error.getStatusCode() : 500;
+      res.status(statusCode).json({ success: false, message: error.message });
+      logger.error(`${req.method} ${req.originalUrl} | ${statusCode} ${error.message}`);
+    } else {
+      const message = `An error occured: ${error.message}`;
+      res.status(500).json({ status: false, message });
+      logger.error(message);
+    }
   } else {
     res.status(400).json({ success: false, message: error.joi.message });
     logger.error(`${req.method} ${req.originalUrl} | ${400} ${error.message}`);
