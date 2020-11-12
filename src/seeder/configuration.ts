032e656091da6af0280e3c558ce590319b3a1e81
Firmansyah Yanuar <fyfirman@gmail.com>
2020-11-12 21:31:33 +0700

    feat: update seeder config

--- src/seeder/configuration.ts
+++ src/seeder/configuration.ts
@@ -1,5 +1,33 @@
 const configData = [
   {
+    configName: 'VOTE_BPM_TI',
+    isActive: true,
+  },
+  {
+    configName: 'VOTE_BPM_TE',
+    isActive: true,
+  },
+  {
+    configName: 'VOTE_BPM_MAT',
+    isActive: true,
+  },
+  {
+    configName: 'VOTE_BPM_FIS',
+    isActive: true,
+  },
+  {
+    configName: 'VOTE_BPM_KIM',
+    isActive: true,
+  },
+  {
+    configName: 'VOTE_BPM_BIO',
+    isActive: true,
+  },
+  {
+    configName: 'VOTE_BPM_AKTU',
+    isActive: true,
+  },
+  {
     configName: 'VOTE_KAHIM_TI',
     isActive: true,
   },
@@ -29,6 +57,10 @@ const configData = [
   },
   {
     configName: 'VOTE_KAHIM_STAT',
+    isActive: true,
+  },
+  {
+    configName: 'VOTE_KABEM',
     isActive: true,
   },
 ];
